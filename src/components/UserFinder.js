0a8f8baef0c0e542f25a172ba7e66b2d5f2cc697
Colson <colsoncalifornication@gmail.com>
2024-02-20 00:28:21 +0545

    implement component lifecycle to deal with sideeffects

--- src/components/UserFinder.js
+++ src/components/UserFinder.js
@@ -1,4 +1,4 @@
-import { Fragment, useState, useEffect } from 'react';
+import { Fragment, useState, useEffect, Component } from 'react';
 
 import Users from './Users';
 import classes from './UserFinder.module.css';
@@ -9,6 +9,56 @@ const DUMMY_USERS = [
   { id: 'u3', name: 'Samridha' },
 ];
 
+class UserFinder extends Component {
+  constructor() {
+    super();
+    this.state = {
+      filteredUsers: DUMMY_USERS,
+      searchTerm: '',
+    };
+
+    this.searchChangeHandler = this.searchChangeHandler.bind(this);
+  }
+
+  // We don't need if check on componentDidMount() because it only run once
+  // when the component initially renders for the first time
+  componentDidMount() {
+    // send http request...
+    // image DUMMY_USERS is fetched from the DB
+    this.setState({ filteredUsers: DUMMY_USERS });
+  }
+
+  // we get last props snapshot and last state snapshot before the
+  // current component update
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.searchTerm !== this.state.searchTerm) {
+      this.setState({
+        filteredUsers: DUMMY_USERS.filter((user) =>
+          user.name.includes(this.state.searchTerm),
+        ),
+      });
+    }
+  }
+
+  searchChangeHandler(event) {
+    // always and object while setting state
+    // it will merge remaining itself like filteredUsers so it wont be lost
+    this.setState({ searchTerm: event.target.value });
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <div className={classes.finder}>
+          <input type='search' onChange={this.searchChangeHandler} />
+        </div>
+        <Users users={this.state.filteredUsers} />
+      </Fragment>
+    );
+  }
+}
+
+/*
 const UserFinder = () => {
   const [filteredUsers, setFilteredUsers] = useState(DUMMY_USERS);
   const [searchTerm, setSearchTerm] = useState('');
@@ -32,5 +82,6 @@ const UserFinder = () => {
     </Fragment>
   );
 };
+*/
 
 export default UserFinder;
